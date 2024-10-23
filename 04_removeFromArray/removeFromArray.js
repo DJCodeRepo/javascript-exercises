7b36ddeb5bec4f68aed9dc813337c372a98d84fa
Danny Hintze <114960227+DJCodeRepo@users.noreply.github.com>
2024-10-23 00:20:30 -0700

    Complete exercises 01-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+
+    const newArr = [];
+    for (let item of arr){
+        let testFlag = Boolean(false);
+        for (let i = 0; i < targets.length; i++){
+            if (item === targets[i]){
+                testFlag = true;
+            }
+        }
+        if (!testFlag) newArr.push(item);
+    }
+    return newArr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
 
+    if (Math.floor(a) !== a || Math.floor(b) !== b){
+        return 'ERROR';
+    }
+
+    if (a < 0 || b < 0){
+        return 'ERROR';
+    }
+
+    if (a > b){
+        const temp = a;
+        a = b;
+        b = temp;
+    }
+
+    for (let i = a; i <= b; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
